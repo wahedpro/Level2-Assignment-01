af57b145cb0df2c31038bdd28b12f55b4a5f2114
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-11-17 19:41:21 +0600

    Problem 3

--- solution.ts
+++ solution.ts
@@ -22,3 +22,22 @@ function getLength(value:string | any[]):number{
     }
     return 0;
 }
+
+// Problem 3:
+class Person{
+    name: string;
+    age: number;
+    constructor(name: string, age:number){
+        this.name=name;
+        this.age=age;
+    }
+    getDetails():string{
+        return `'Name: ${this.name}, Age: ${this.age}'`;
+    }
+}
+
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
