b986f25bdf15c23becce84aea3602331c048bc63
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-11-17 18:58:08 +0600

    problem 2

--- solution.ts
+++ solution.ts
@@ -5,6 +5,20 @@ function formatValue(value:string|number|boolean):string|number|boolean{
     }
     if(typeof value === "number"){
         return value*10;
+    }
+    if(typeof value == "boolean"){
+        return !value
     }  
-    return !value;
+    return value;
+}
+
+// Problem 2:
+function getLength(value:string | any[]):number{
+    if(typeof value === "string"){
+        return value.length;
+    }
+    if(Array.isArray(value)){
+        return value.length;
+    }
+    return 0;
 }
