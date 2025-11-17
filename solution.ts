73ea52d1d358f64942ad6eed99abfbee9d933634
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-11-17 19:42:25 +0600

    remove comment

--- solution.ts
+++ solution.ts
@@ -35,9 +35,3 @@ class Person{
         return `'Name: ${this.name}, Age: ${this.age}'`;
     }
 }
-
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
