9eb78b4d6ac78b6554bbb4d656eb412ead3ed74f
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-11-17 18:39:21 +0600

    problem 1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,10 @@
 // Problem 1:
+function formatValue(value:string|number|boolean):string|number|boolean{
+    if(typeof value ==="string"){
+        return value.toUpperCase();
+    }
+    if(typeof value === "number"){
+        return value*10;
+    }  
+    return !value;
+}
