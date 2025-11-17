bb045609eb501ac72ae9ccafeac0be0aa9dd6755
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-11-17 22:27:47 +0600

    Problem 6

--- solution.ts
+++ solution.ts
@@ -58,3 +58,18 @@ function filterActiveUsers(users: User[]):User[]{
     return users.filter(user=>user.isActive===true);
 }
 
+// Problem 6:
+interface Book{
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book):void{
+    const availability = book.isAvailable ? "Yes" : "No";
+    console.log(
+        `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
+    );
+}
+
